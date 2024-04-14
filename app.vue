9c1c00e1b5b980bca3625294d093f24664361f01
Cecile <stav0015@algonquinlive.com>
2024-04-14 17:51:04 -0400

    created nuxt frontend for strapi headless cms api

---
+++ app.vue
@@ -0,0 +1,48 @@
+<template>
+  
+  <PostList :posts="data" />
+
+</template>
+
+<script setup>
+
+  const API_URL = 'https://mtm6407-headlesscms-backend.onrender.com/api/posts?populate[image][fields][0]=url&populate[authors][fields][1]=firstName&populate[authors][fields][2]=lastName&populate[authors][fields][3]=email';
+
+  const { data } = await useFetch(API_URL);
+
+//  add to head, from https://github.com/lostmart/quick-nuxt3-bootstrap5.2-integration
+
+useHead({
+  title: "Epic Reviews",
+  link: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+      rel: "stylesheet",
+      integrity: "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH",
+      crossorigin: "anonymous"
+    },
+    {
+      href: "https://kit.fontawesome.com/a369465566.js",
+      crossorigin: "anonymous"
+    }
+  ],
+  script: [
+    {
+      src: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
+      integrity: "sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r",
+      crossorigin: "anonymous"
+    },
+    {
+      src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js",
+      integrity: "sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy",
+      crossorigin: "anonymous"
+    }
+  ]
+
+})
+
+</script>
+
+<style>
+
+</style>
---
+++ components/PostList.vue
@@ -0,0 +1,59 @@
+<template>
+
+ <div class="postlist-container container d-flex gap-4">
+    <div v-for="post in posts.data">       
+
+        <div class="post-card card" style="width: 19rem;">
+            <img class="card-img-top postlist-img img-fluid" :src="post.attributes.image.data.attributes.url" :alt="post.attributes.title">
+            <div class="card-body d-flex flex-column">
+                <h5 class="card-title">{{post.attributes.title}}</h5>
+                <p class="card-text">{{ post.attributes.summary }}</p>
+                <a href="#" class="btn btn-primary align-self-start mt-auto">Go somewhere</a>
+        </div>
+</div>
+       
+
+    </div>
+ </div>
+
+</template>
+
+<script setup>
+
+defineProps({
+    posts: Object
+})
+
+</script>
+
+<style scoped>
+
+/* .card-body {
+    height: 300px;
+} */
+
+
+    .card-body p {
+    overflow: hidden;
+    max-width: 100%;
+    text-overflow: ellipsis;
+    width: fit-content;
+    display: -webkit-box;
+   -webkit-line-clamp: 4; /* number of lines to show */
+           line-clamp: 2;
+   -webkit-box-orient: vertical;
+    }
+
+    .post-card {
+        height: 440px;
+    }
+
+    .postlist-img {
+        max-width: 385px;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        font-family: 'Inter', 'sans-serif';
+    }
+
+</style>
